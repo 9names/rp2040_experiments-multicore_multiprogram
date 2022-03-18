f48ec86848895d8caf85397676cae9ab6cea0faf
9names <60134748+9names@users.noreply.github.com>
2022-03-18 15:35:22 +1100

    Clean up core1 flash example

--- core0-flash_core1-flash/src/main.rs
+++ core0-flash_core1-flash/src/main.rs
@@ -1,10 +1,12 @@
-//! Blinks the LED on a Pico board
+//! Launch a precompile program from flash on core1
 //!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+//! This will launch the program `core1-blinky-flash` on core1.
+//! This program returns reads from the multicore fifo, and then writes that value + 1
+//! Yes, that program is poorly named.
+
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
@@ -16,6 +18,7 @@ use panic_probe as _;
 use rp_pico as bsp;
 
 use bsp::hal::{
+    entry,
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
@@ -27,72 +30,6 @@ use bsp::hal::{
 #[link_section = ".core1"]
 #[used]
 pub static CORE1: [u8; 652] = *include_bytes!("../../core1-blinky-flash/core1.bin");
-
-fn boot_core1(psm: &mut pac::PSM, ppb: &mut pac::PPB, sio: &mut bsp::hal::Sio) {
-    let vector_table = 0x10020000;
-    let stack_ptr = 0x20030000 - 0x8;
-    let reset_vector = 0x10020004 as *const u32;
-    let entry = unsafe { reset_vector.read_volatile() };
-
-    //let (psm, ppb, sio) = (&mut pac.PSM, &mut pac.PPB, &mut sio);
-    // Reset the core
-    psm.frce_off.modify(|_, w| w.proc1().set_bit());
-    while !psm.frce_off.read().proc1().bit_is_set() {
-        cortex_m::asm::nop();
-    }
-    // Drain the fifo before letting the core out of reset
-    sio.fifo.drain();
-    psm.frce_off.modify(|_, w| w.proc1().clear_bit());
-    // We should get a 0 back from core1
-    info!("core1 let out of reset");
-    let read = sio.fifo.read_blocking();
-    if read != 0 {
-        info!("core1 sent something unexpected");
-    }
-    // After reset, core 1 is waiting to receive commands over FIFO.
-    // This is the sequence to have it jump to some code.
-    let cmd_seq = [
-        0,
-        0,
-        1,
-        vector_table as usize,
-        stack_ptr as usize,
-        entry as usize,
-    ];
-
-    let mut seq = 0;
-    let mut fails = 0;
-    loop {
-        // info!("core1 start loop");
-        let cmd = cmd_seq[seq] as u32;
-        info!("sending {:X} to core1", cmd);
-        // Always drain the READ FIFO (from core 1) before sending a 0
-        if cmd == 0 {
-            sio.fifo.drain();
-            // Execute a SEV as core 1 may be waiting for FIFO space via WFE
-            cortex_m::asm::sev();
-        }
-        sio.fifo.write_blocking(cmd);
-        let response = sio.fifo.read_blocking();
-        info!("core1 sent {:X} in response", response);
-        if cmd == response {
-            // Move to next state on correct response (echo-d value)
-            seq += 1;
-        } else {
-            // otherwise start over
-            seq = 0;
-            fails += 1;
-            if fails > 16 {
-                info!("failed write");
-                break;
-            }
-        }
-        if seq >= cmd_seq.len() {
-            info!("seq >= cmd_seq.len()");
-            break;
-        }
-    }
-}
 
 #[entry]
 fn main() -> ! {
@@ -118,14 +55,17 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
-    // let pins = bsp::Pins::new(
-    //     pac.IO_BANK0,
-    //     pac.PADS_BANK0,
-    //     sio.gpio_bank0,
-    //     &mut pac.RESETS,
-    // );
+    let mut mc = bsp::hal::multicore::Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio);
+    let cores = mc.cores();
+    let core1 = &mut cores[1];
 
-    //let mut led_pin = pins.led.into_push_pull_output();
+    let core1_program_started = unsafe { core1.bootload(0x10020000) };
+    info!("Core 1 started okay? {:?}", core1_program_started.is_ok());
+
+    let read = sio.fifo.read();
+    if let Some(read) = read {
+        info!("Core1 sent {} after start", read);
+    }
 
     // Set the pins to their default state
     let pins = bsp::hal::gpio::Pins::new(
@@ -136,18 +76,10 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
-    led_pin.set_high();
+    let _ = led_pin.set_high();
 
-    let SIO2: bsp::hal::pac::SIO = unsafe { core::mem::transmute(()) };
-    let mut sio2 = Sio::new(SIO2);
-    boot_core1(&mut pac.PSM, &mut pac.PPB, &mut sio2);
-    let read = sio.fifo.read();
-    if let Some(read) = read {
-        info!("Core1 send {}", read);
-    }
-    info!("Program start");
-    sio.fifo.write_blocking(b'a' as u32);
-    sio.fifo.write_blocking(b'a' as u32);
+    info!("Main loop start");
+
     let mut count: u32 = 0;
     loop {
         cortex_m::asm::sev();
@@ -158,10 +90,10 @@ fn main() -> ! {
             info!("Core1 send {}", read);
         }
         info!("on!");
-        //led_pin.set_high().unwrap();
+        led_pin.set_high().unwrap();
         delay.delay_ms(500);
         info!("off!");
-        //led_pin.set_low().unwrap();
+        led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
